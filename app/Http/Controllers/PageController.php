5e8f568a3406293f8cae5c763998a444955abed7
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-04 02:03:49 +0700

    Uji coba dengan memodifikasi fungsi index

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -14,13 +14,19 @@ class PageController extends Controller
     //     ]);
     // }
 
+    // public function index()
+    // {
+    //     $games = GameInfo::all();
+    //     // dd($games); // Cek apakah data berhasil diambil dari database
+    //     return view('page', [
+    //         "games" => $games
+    //     ]);
+    // }
+
     public function index()
     {
-        $games = GameInfo::all();
-        dd($games); // Cek apakah data berhasil diambil dari database
-        return view('page', [
-            "games" => $games
-        ]);
+        // dd($games); // Cek apakah data berhasil diambil dari database
+        return view('page', GameInfo::all());
     }
 }
 
