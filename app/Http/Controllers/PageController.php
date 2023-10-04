3a6671680a3b3b59ddc580330de5a6a63edcc823
Arfi23 <arfifaizilraushani@gmail.com>
2023-10-04 08:27:58 +0700

    Merapikan dan memperbaiki ProgramController

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,26 +7,15 @@ use App\Models\GameInfo;
 
 class PageController extends Controller
 {
-    // public function index()
-    // {
-    //     return view('page', [
-    //         "games" => GameInfo::all()
-    //     ]);
-    // }
-
-    // public function index()
-    // {
-    //     $games = GameInfo::all();
-    //     // dd($games); // Cek apakah data berhasil diambil dari database
-    //     return view('page', [
-    //         "games" => $games
-    //     ]);
-    // }
 
     public function index()
     {
+        $games = GameInfo::all();
         // dd($games); // Cek apakah data berhasil diambil dari database
-        return view('page', GameInfo::all());
+        return view('page', [
+            "games" => $games
+        ]);
+        
     }
 }
 
