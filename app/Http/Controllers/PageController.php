bcfe09a99ade38c13981faf1bed365ca498d8bd6
Arfi23 <arfifaizilraushani@gmail.com>
2023-11-01 16:24:24 +0700

    Mencoba menerapkan cache

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,25 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\GameInfo;
+use Illuminate\Support\Facades\Cache;
 
 class PageController extends Controller
 {
 
     public function index()
     {
-        $games = GameInfo::all();
+        $time_start = microtime(true);
+
+        // $games = GameInfo::all();
         // dd($games); // Cek apakah data berhasil diambil dari database
+        for ($i = 0; $i < 100; $i++) {
+            if (Cache::has('game')) {
+                $games = Cache::get('game');
+            } else {
+                $games = GameInfo::all();
+                Cache::put('game', $games, 60);
+            }
+        }
         return view('page', [
             "games" => $games
         ]);
-        
     }
 }
-
-
-
-
-
-
-
-
